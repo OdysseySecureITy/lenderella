d7a6e77f77529586c3e05811757531d44a353d06
LitvinovNikita <nlitvinov924@gmail.com>
2025-06-04 20:39:16 -0400

    some edits downgraded express from v5 to v4.21.2

--- server/server.mjs
+++ server/server.mjs
@@ -69,4 +69,5 @@ const distPath = path.join(__dirname, "../dist");
 app.use(express.static(distPath));
 app.get("*", (req, res) => res.sendFile(path.join(distPath, "index.html")));
 
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
