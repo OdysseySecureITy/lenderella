6d915caad21c445819b89c580f57e0509fc5032f
LitvinovNikita <nlitvinov924@gmail.com>
2025-06-06 01:10:54 -0400

    updated from in server

--- server/server.mjs
+++ server/server.mjs
@@ -65,7 +65,7 @@ app.post("/api/contact", async (req, res) => {
 
   const msg = {
     to: "info@incredifund.com",
-    from: "em6111.incredifund.com",
+    from: "info@em6111.incredifund.com",
     subject: `New message from ${name}`,
     text: `
       Name: ${name}
